3f414f5bc3a12f948843219a39dae1b4b1748950
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-05-24 11:58:14 +0300

    Initial Commit

---
+++ Recipe/Controllers/StartScreenViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StartScreenViewController.swift
+//  Recipe
+//
+//  Created by Amr El-Fiqi on 24/05/2023.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+    
+    // MARK: - Labels, Buttons and Images
+    
+    private let backgroundImage = UIImage(named: "startingScreen")
+    
+    private let chefIcon = UIImage(named: "chefHat")
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+}
