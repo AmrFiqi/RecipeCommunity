8b28f3a610a8910021a0c4f8bb3f13dfe70681fc
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-05-27 21:18:49 +0300

    Created the bottom part of the screen with the button

--- Recipe/Controllers/StartScreenViewController.swift
+++ Recipe/Controllers/StartScreenViewController.swift
@@ -17,7 +17,8 @@ class StartScreenViewController: UIViewController {
         super.viewDidLoad()
 
         setupBackground()
-        setupUiComponent()
+        setupTopUIComponents()
+        setupBottomUIComponents()
     }
  
     private func setupBackground() {
@@ -29,7 +30,7 @@ class StartScreenViewController: UIViewController {
         view.sendSubviewToBack(backgroundImageView)
     }
     
-    private func setupUiComponent() {
+    private func setupTopUIComponents() {
         let chefIcon = UIImage(named: "chefHat")
         let imageView = UIImageView(image: chefIcon)
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -44,7 +45,7 @@ class StartScreenViewController: UIViewController {
         view.addSubview(topLabel)
         
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 104),
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.heightAnchor.constraint(equalToConstant: 79),
             imageView.widthAnchor.constraint(equalToConstant: 79),
@@ -53,5 +54,49 @@ class StartScreenViewController: UIViewController {
             topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
         
+    }
+    
+    private func setupBottomUIComponents() {
+        let startCookingButton = UIButton(type: .system)
+        startCookingButton.setTitle("Start Cooking ->", for: .normal)
+        startCookingButton.applyButtonStyle(.primary)
+        startCookingButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(startCookingButton)
+        
+        let bottomLabel = UILabel()
+        bottomLabel.text = "Simple Way to find Tasty Recipe"
+        bottomLabel.textAlignment = .center
+        bottomLabel.textColor = .white
+        bottomLabel.font = UIFont.systemFont(ofSize: bottomLabel.font.pointSize, weight: .semibold)
+        bottomLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomLabel)
+        
+        let mainLabel = UILabel()
+        mainLabel.text = "Get Cooking"
+        mainLabel.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        mainLabel.textAlignment = .center
+        mainLabel.textColor = .white
+        mainLabel.numberOfLines = 0
+        mainLabel.adjustsFontSizeToFitWidth = true
+        mainLabel.minimumScaleFactor = 0.5
+        mainLabel.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainLabel)
+        
+        
+        NSLayoutConstraint.activate([
+            startCookingButton.topAnchor.constraint(equalTo: view.bottomAnchor, constant: -150),
+            startCookingButton.widthAnchor.constraint(equalToConstant: 234),
+            startCookingButton.heightAnchor.constraint(equalToConstant: 54),
+            startCookingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            bottomLabel.topAnchor.constraint(equalTo: startCookingButton.topAnchor, constant: -64),
+            bottomLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            mainLabel.bottomAnchor.constraint(equalTo: bottomLabel.topAnchor, constant: -20),
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLabel.widthAnchor.constraint(equalToConstant: 213),
+            mainLabel.heightAnchor.constraint(equalToConstant: 120),
+        ])
     }
 }
